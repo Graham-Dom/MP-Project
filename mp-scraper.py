7c7d8123cf4dec204847ff703269c0e26e3ca451
Graham Dominick <graham.dom17@gmail.com>
2019-01-17 21:29:42 -0700

    Finished Scraper and got rid of route and grade datatype

--- mp-scraper.py
+++ mp-scraper.py
@@ -1,43 +1,75 @@
-import requests
+import sys
+import csv
 import time
+import requests
 from bs4 import BeautifulSoup
 
+def collect_routes(area, writer):
 
-def collect_routes(area):
-
-	routes = area.select(".mp-sidebar tr")
-	location = ">".join([link.text for link in area.select(".mb-half > a")][1:])
-
-	for route in routes:
-
-		name = route.a.text
-		grade = route.select_one(".rateYDS").text
-		types = route.select_one(".route-type").attrs["class"][1:]
-		rating = float(len(route.select("img")))
-		if "Half" in route.select("img")[-1].get("src"):
-			rating -= 0.5
-
-		print(name, grade, types, rating)
-
-def crawl_from_area(URL):
-
-	# Don't overload their servers!
-	time.sleep(10)
-
-	area = BeautifulSoup(requests.get(URL).text, "html.parser")
-
-	# If there are sub areas, crawl each of them
-	if "Areas" in area.select_one(".mp-sidebar > h3").string:
-
-		for sub_area in area.select(".lef-nav-row > a"):
-			crawl_from_area(sub_area.get("href"))
-
-	else:
-		collect_routes(area)
+    routes = area.select('.mp-sidebar tr')
+    location = '>'.join([link.text for link in area.select('.mb-half > a')][1:] 
+        + [area.select_one('.mp-sidebar > h3').string[10:]])
 
 
+    for route in routes:
+
+        # Screen for non-route elements in sidebar
+        if route.has_attr('id'):
+            continue
+
+        # Only scrape rock routes
+        yds = route.select_one('.rateYDS')
+        if yds:
+            grade = yds.text
+        else:
+            continue
+
+        name = route.a.text
+        type = ', '.join(route.select_one('.route-type').attrs['class'][1:])
+        rating = float(len(route.select('img')))
+        if 'Half' in route.select('img')[-1].get('src'):
+	        rating -= 0.5
+
+        writer.writerow({
+            'Name'    : name,
+            'Type'    : type,
+            'Grade'   : grade,
+            'Rating'  : rating,
+            'Location': location
+        })
 
 
-if __name__ == "__main__":
+def crawl_from_area(URL, writer):
 
-	crawl_from_area("https://www.mountainproject.com/area/105810440/second-tier")
+    # Don't overload their servers!
+    time.sleep(10)
+
+    area = BeautifulSoup(requests.get(URL).text, 'html.parser')
+
+    # If there are sub areas, crawl each of them
+    area_info = area.select_one('.mp-sidebar > h3')
+    if area_info: 
+
+        if 'Areas' in area_info.string:
+            for sub_area in area.select('.lef-nav-row > a'):
+            	crawl_from_area(sub_area.get('href'), writer)
+
+        elif 'Routes' in area_info.string:
+            collect_routes(area, writer)
+
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        url = sys.argv[1]
+        with open('mp-crawl-output.csv', 'w', newline = '') as csvfile:
+            fieldnames = ['Name', 'Type', 'Grade', 'Rating', 'Location']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            crawl_from_area(url, writer)
+    else:
+        print("Please enter a mountain project area URL to search from")
+
+     
+
--- route.py
+++
@@ -1,6 +0,0 @@
-class ydsGrade:
-
-	def __init__(self, grade):
-		self.number_grade = number_grade
-
-	num
